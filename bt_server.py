344589edb45df0bfb18129ac872c702d232aaa8c
Rick Bischoff <rdbisch@gmail.com>
2021-03-06 07:27:29 -0800

    modified server to take more commands; abstracted command parsing

--- bt_server.py
+++ bt_server.py
@@ -14,6 +14,21 @@ OK = "000111001101111 binary solo" # The humans are dead
 def X(s):
 	return str(s).encode('utf-8')
 
+commands = {
+	"set_angle": (Car.set_angle, 'f'),
+	"read_angle": (Car.read_angle, None),
+	"set_heading": (Car.set_heading, 'f'),
+	"read_heading": (Car.read_velocity, None),
+	"drive_forwards": (Car.drive_forwards, None),
+	"drive_backwards": (Car.drive_backwards, None),
+	"ping": (Car.ping, None),
+	"take_picture": (Car.take_picture, None)		
+}
+
+argparse = {
+	'f': lambda x: float(x)
+}
+
 print("listening on port ", port)
 client, clientInfo = s.accept()
 while 1:
@@ -26,14 +41,20 @@ while 1:
 		print("Received data {0} args {1}".format(data, args))
 		command = args[0]
 
-		if command == 'setservo':
-			print("setservo received. angle {0}".format(args[1]))
-			angle = float(args[1])
-			Car.set_angle(angle)
-			client.send(X(OK))
-		elif command == 'readservo':
-			client.send(X(Car.read_angle()))
-			client.send(X(OK))
+		if command in commands:
+			func, arg = commands[command]
+			if arg in argparse: 
+				parsed_arg = argparse[arg](args[1])
+				client.send("Received command {0} with arg {1}".format(func, parsed_arg))
+				result = func(parsed_arg)
+				client.send(result)
+			elif arg == None:
+				client.send("Received comamnd {0}".format(func))
+				result = func()
+				client.send(result)
+			else:
+				raise "Invalid argument {0} for function {1}".format(arg, func)
+
 		else:
 			client.send("unknown command.")
-			client.send(OK) # Echo back to client
+
